5cbd6378338192c3089922ba93a2d69ac5708740
arturo.manzaneda <arturo.manzaneda.tudela@ing.com>
2019-01-24 15:38:10 +0100

    Add docker-compose default values + publish docker script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-envsubst '$${API_HOST},$${API_PORT},$${NGINX_HOST},$${NGINX_PORT}' < /etc/nginx/conf.d/reverse.conf > /etc/nginx/conf.d/default.conf
+envsubst '$${IDC_API_DOMAIN_PROTOCOL},$${IDC_API_DOMAIN_HOST},$${IDC_API_DOMAIN_PORT},$${NGINX_HOST},$${NGINX_PORT}' < /etc/nginx/conf.d/reverse.conf > /etc/nginx/conf.d/default.conf
 rm /etc/nginx/conf.d/reverse.conf
 
 # run docker CMD
--- publish-docker.sh
+++ publish-docker.sh
@@ -6,13 +6,13 @@ docker login
 VERSION=`cat ./.dockerHubVersion`
 echo "Next Version? (current: "${VERSION}")"
 read nversion
-echo "Let's build new image with tag: idchain/test-pool:"${nversion}
-docker build . -t idchain/test-pool:${nversion}
-docker push idchain/test-pool:${nversion}
+echo "Let's build new image with tag: idchain/admin-ui:"${nversion}
+docker build . -t idchain/admin-ui:${nversion}
+docker push idchain/admin-ui:${nversion}
 echo ${nversion} > ./.dockerHubVersion
 
 git add ./.dockerHubVersion
-msg=`echo New test-pool image release version: ${nversion}`
+msg=`echo New admin-ui image release version: ${nversion}`
 echo ${msg}
 git commit -m "$msg"
 git push
